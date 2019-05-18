771ed0b6f4e95477beb58967fade400de37a5d23
Marsm <socketming@163.com>
2019-05-19 01:03:17 +0800

    .....

--- pysparkProject/pyspark_wordcount.py
+++ pysparkProject/pyspark_wordcount.py
@@ -13,8 +13,8 @@ file = sc.textFile('./data/word.txt')
 # 获取每行数据并将每个单词打个标签1
 word = file.flatMap(lambda x: x.split(',')).map(lambda a: (a, 1))
 # 对每个单词计数，并按单词出现的次数降序排序
-words = word.reduceByKey(lambda a,b:a+b).sortBy(lambda x:x[1],ascending=False)
-for k,v in words.collect():
-    print('{0}:{1}'.format(k,v))
+words = word.reduceByKey(lambda a, b: a + b).sortBy(lambda x: x[1], ascending=False)
+for k, v in words.collect():
+    print('{0}:{1}'.format(k, v))
 
 sc.stop()
